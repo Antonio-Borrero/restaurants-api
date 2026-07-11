97a81b7dcb721a88c5ad944d92d2f9d8bd412030
Antonio <antojoseabad@gmail.com>
2026-07-11 14:02:46 +0200

    feat(restaurants): connect Prisma client and add endpoint to create restaurants

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { prisma } from "./lib/prisma.ts";
 
 const app = express();
 
@@ -6,6 +7,28 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.json({ mensaje: "API funcionando" });
+});
+
+app.post("/restaurants", async (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res.status(400).json({ error: "Faltan datos obligatorios" });
+	}
+	if (typeof name !== "string") {
+		return res
+			.status(400)
+			.json({ error: "Los datos deben ser de tipo string" });
+	}
+	try {
+		const newRestaurant = await prisma.restaurant.create({
+			data: {
+				name,
+			},
+		});
+		res.status(201).json(newRestaurant);
+	} catch (error) {
+		res.status(500).json({ error: "Error al crear el restaurante" });
+	}
 });
 
 export default app;
